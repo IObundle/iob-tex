0ab4ee4d78f97fd4c509bd3eae81eb9e7bf14d6f
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2020-09-08 20:25:27 +0100

    Updated v2tex.py to support macros, 'reg' and 'signed'

--- software/v2tex.py
+++ software/v2tex.py
@@ -8,7 +8,7 @@ import os.path
 import re
 import string
 
-def parse (program) :
+def parse (program, defines) :
     program_out = []
     for line in program :
         flds_out = ['','','','']
@@ -18,32 +18,117 @@ def parse (program) :
         if (flds[0] != 'input') & (flds[0] != 'output') & (flds[0] != 'inout'): continue #not an IO
         #print flds
         flds_out[1] = flds[0] #signal direction
+
+        flds_w = 1
+        if (flds[1] == 'reg'):
+            flds_w = flds_w+1
+        if (flds[1] == 'signed'):
+            flds_w = flds_w+1
+            
         if not('[' in line):
-            flds_out[0] = re.sub('_','\\_',flds[1]) #signal name
+            flds_out[0] = re.sub('_','\\_',flds[flds_w]) #signal name
             flds_out[2] = '1' #signal width
-            flds_out[3] = string.join(flds[2:]) #signal description
+            flds_out[3] = string.join(flds[flds_w+1:]) #signal description
         else:
-            flds_out[0] = re.sub('_','\\_',flds[3]) #signal name
-            flds_out[2] = str(int(flds[1]) - int(flds[2]) + 1)  #signal width
-            flds_out[3] = string.join(flds[4:]) #signal description
+            flds_out[0] = re.sub('_','\\_',flds[flds_w+2]) #signal name
+            for key, val in defines.items():
+                if key in str(flds[flds_w]):
+                    flds[flds_w] = eval(re.sub(str(key),str(val), flds[flds_w]))
+                if key in str(flds[flds_w+1]):
+                    flds[flds_w+1] = eval(re.sub(str(key),str(val), flds[flds_w+1]))
+                pass
+            flds_out[2] = str(int(flds[flds_w]) - int(flds[flds_w+1]) + 1)  #signal width
+            flds_out[3] = string.join(flds[flds_w+3:]) #signal description
         program_out.append(flds_out)
 
          
     return program_out
 
-            
+def header_parse (f):
+    defines = {}
+    for line in f:
+        #ignore leading spaces
+        line = line.lstrip() 
+        
+        #replace $clog2 by clog2
+        line = line.replace("$", "")
+        
+        #remove tabs
+        line = line.replace("\t", "")
+        
+        #split line elements in array
+        line = re.sub(' +', ' ', line)
+        values = line.split("\n")
+        values = values[0].split(" ")
+        
+        #parser
+        lookup = {}
+        if "`define" == values[0]:
+            if(len(values) > 2):
+                if values[2].isdigit() == False:
+                    if "'d" in line:
+                        const = line.split("'d")
+                        const = const[1].split(" ")
+                        const[0] = re.search(r'\d+', const[0]).group()
+                        lookup["`" + values[1]] = int(const[0],10)
+                    elif "'h" in line:
+                        const = line.split("'h")
+                        const = const[1].split(" ")
+                        lookup["`" + values[1]] = int(const[0],16)
+                    elif "(" in line:
+                        line = re.sub('`', '', line)
+                        if "'b" in line:
+                            line = re.sub('1\'b', '', line)
+                        if '**' in line:
+                            line = re.sub('2\*\*', '(1<<', line)
+                            line = re.sub(re.escape(' +'), ') +', line)
+                            line = re.sub(re.escape('-1)'), ')-1', line)
+                            line = re.sub(re.escape('-2)'), ')-2', line)
+                        const = line.split("(", 1)
+                        const = const[1].split("//")
+                        const = const[0].split('\n')
+                        const = "(" + const[0] 
+                        lookup["`" + values[1]] = const
+                    elif "{" in line:
+                        continue
+                    else:
+                        const = values[2].split("`")
+                        const = const[1].split("\n")
+                        lookup["`" + values[1]] = lookup[const[0]]
+                else:
+                    lookup["`" + values[1]] = int(values[2])
+            else:
+                lookup[values[0] + " " + values[1]] = ""
+        else:
+            continue
+
+        #Write to dictionary
+        for key, val in lookup.items():
+            defines.update({key:int(val)})
+    return defines
+
 def main () :
     #parse command line
-    if len(sys.argv) != 3:
-        vaError("Usage: ./v2tex.py infile outfile define_string")
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        vaError("Usage: ./v2tex.py infile outfile [header_file]")
     else:
         infile = sys.argv[1]
         outfile = sys.argv[2]
+        if len(sys.argv) == 4:
+            vhfile = sys.argv[3]
+        pass
 
+    defines = {}
+    if 'vhfile' in locals():
+        #Create header dictionary
+        fvh = open(vhfile, 'r')
+        defines = header_parse(fvh)
+        fvh.close()
+        
     #parse input file
     fin = open (infile, 'r')
     program = fin.readlines()
-    program = parse (program)
+    program = parse (program, defines)
 
     #print program
     #for line in range(len(program)):
@@ -55,5 +140,9 @@ def main () :
         if ((i%2) != 0): fout.write("\\rowcolor{iob-blue}\n")
         line = program[i]
         fout.write('%s & %s & %s & %s \\\ \hline\n' %  (line[0], line[1], line[2], line[3]));
+
+    #Close files
+    fin.close()
+    fout.close()
 
 if __name__ == "__main__" : main ()
