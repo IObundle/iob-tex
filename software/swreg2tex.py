a007e9f680804f5f30134851ee9318121849849a
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2020-09-14 14:37:56 +0100

    swreg2tex now reads a single .v to generate all tables

--- software/swreg2tex.py
+++ software/swreg2tex.py
@@ -11,75 +11,7 @@ import math
 
 from vhparser import header_parse
 
-def swreg_parse (program, defines) :
-    program_out = []
-    swreg_list = []
-    for line in program :
-        subline = re.sub('\[|\]|:|,|//|\;',' ', line)
-        subline = re.sub('\(',' ',subline, 1)
-        subline = re.sub('\)',' ', subline, 1)
-
-        flds = subline.split()
-        if not flds : continue #empty line
-        #print flds[0]
-        if ('SWREG_' in flds[0]): #software accessible registers
-            swreg_list.append(flds)
-        else: continue #not a recognized macro
-
-    #generate software accessible register table
-    if swreg_list:
-        addr_w = int(math.ceil(math.log(len(swreg_list)*4)/math.log(2)/4))
-        swreg_addr = 0
-        for flds in swreg_list:
-            flds_out = ['','','','','']
-            flds_out[0] = re.sub('_','\\_', flds[1]) #register name
-
-            #register direction
-            if '_RW' in flds[0]:
-                flds_out[1] = 'R/W'
-            elif 'W' in flds[1]:
-                flds_out[1] = 'W'
-            else:
-                flds_out[1] = 'R'
-
-            flds_out[2] = ("0x{:0" + str(addr_w) + "x}").format(swreg_addr) #register addr
-            swreg_addr = swreg_addr+4
-            for key, val in defines.items():
-                if key in str(flds[2]):
-                    flds[2] = eval(re.sub(str(key),str(val), flds[2]))
-            flds_out[3] = str(int(flds[2])-1) + ":0" #register width
-
-            flds_out[4] =  string.join(flds[3:]) #register description
-
-            program_out.append(flds_out)
-
-    return program_out
-
-def main () :
-    #parse command line
-    if len(sys.argv) != 3 and len(sys.argv) != 4:
-        vaError("Usage: ./v2tex.py infile outfile [header_file]")
-    else:
-        infile = sys.argv[1]
-        outfile = sys.argv[2]
-        if len(sys.argv) == 4:
-            vhfile = sys.argv[3]
-        pass
-
-    defines = {}
-    if 'vhfile' in locals():
-        #Create header dictionary
-        defines = header_parse(vhfile)
-        
-    #parse input file
-    fin = open (infile, 'r')
-    program = fin.readlines()
-    program = swreg_parse (program, defines)
-
-    #print program
-    #for line in range(len(program)):
-     #   print program[line]
-
+def write_table(outfile, program):
     #write output file
     fout = open (outfile, 'w')
     for i in range(len(program)):
@@ -90,8 +22,87 @@ def main () :
             line_out = line_out + (' & %s' % line[l])
         fout.write(line_out + ' \\\ \hline\n')
 
-    #Close files
-    fin.close()
     fout.close()
+    return
+
+
+def swreg_parse (program, defines) :
+    swreg_cnt = 0
+    tables_dict = {}
+    for line in program :
+        subline = re.sub('\[|\]|:|,|//|\;',' ', line)
+        subline = re.sub('\(',' ',subline, 1)
+        subline = re.sub('\)',' ', subline, 1)
+
+        flds = subline.split()
+        if not flds : continue #empty line
+        #print flds[0]
+        if ('START_TABLE' in flds[0]):
+            table_name = flds[1]
+            table_swregs = []
+            tables_dict.update({table_name:table_swregs})
+        elif ('SWREG_' in flds[0]): #software accessible registers
+            tables_dict[table_name].append(flds)
+            swreg_cnt = swreg_cnt + 1
+        else: continue #not a recognized macro
+
+    #generate software accessible register table
+    if tables_dict:
+        print(tables_dict)
+        addr_w = int(math.ceil(math.log(swreg_cnt*4)/math.log(2)/4))
+        swreg_addr = 0
+        for table_name, swreg_list in reversed(tables_dict.items()):
+            program_out = []
+            for flds in swreg_list:
+                flds_out = ['','','','','']
+                flds_out[0] = re.sub('_','\\_', flds[1]) #register name
+
+                #register direction
+                if '_RW' in flds[0]:
+                    flds_out[1] = 'R/W'
+                elif 'W' in flds[1]:
+                    flds_out[1] = 'W'
+                else:
+                    flds_out[1] = 'R'
+
+                flds_out[2] = ("0x{:0" + str(addr_w) + "x}").format(swreg_addr) #register addr
+                swreg_addr = swreg_addr+4
+                for key, val in defines.items():
+                    if key in str(flds[2]):
+                        flds[2] = eval(re.sub(str(key),str(val), flds[2]))
+                flds_out[3] = str(int(flds[2])-1) + ":0" #register width
+
+                flds_out[4] =  string.join(flds[3:]) #register description
+
+                program_out.append(flds_out)
+
+            write_table(str(table_name) + '_tab.tex', program_out)
+
+    return
+
+def main () :
+    #parse command line
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        vaError("Usage: ./v2tex.py infile [header_file]")
+    else:
+        infile = sys.argv[1]
+        if len(sys.argv) == 3:
+            vhfile = sys.argv[2]
+        pass
+
+    defines = {}
+    if 'vhfile' in locals():
+        #Create header dictionary
+        defines = header_parse(vhfile)
+        
+    #parse input file
+    fin = open (infile, 'r')
+    program = fin.readlines()
+    fin.close()
+    swreg_parse (program, defines)
+
+    #print program
+    #for line in range(len(program)):
+     #   print program[line]
 
 if __name__ == "__main__" : main ()
