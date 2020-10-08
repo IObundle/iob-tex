d8fd8a88c43f000968e4d5543d5e1c9884b59875
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-10-08 19:19:22 +0100

    repository is read only, no execution

--- document/document.mk
+++ document/document.mk
@@ -1,46 +1,59 @@
-include $(CORE_DIR)/core.mk
-
-pb: pb.pdf
-
-ug: ug.pdf
-
 EXPORT_LIST:=\
-COMPILE_SERVER=$(COMPILE_SERVER) \
-REMOTE_ROOT_DIR=$(REMOTE_ROOT_DIR) \
 INTEL=$(INTEL) \
 XILINX=$(XILINX) \
 CORE_NAME=$(CORE_NAME) \
-TEX_DIR=$(TEX_DIR)
+TEX_DIR=$(TEX_DIR)/document
+
+TEX_SRC:= color.tex deliverables.tex benefits.tex
+TEX_FIG:= bg.odg Logo.png
+
 HW_DIR:=$(CORE_DIR)/hardware
-SW_DIR:=$(TEX_DIR)/../software
+
+XIL_LOG:=vivado.log
+INT_LOG:=*.fit.summary
 
 figures:
+	cp $(addprefix $(TEX_DIR)/document/figures/, $(TEX_FIG)) ../figures
 	make -C ../figures
 
-fpga: vivado.log $(CORE_NAME).fit.summary
-	$(EXPORT_LIST) $(SW_DIR)/fpga2tex.sh
-
-$(CORE_NAME).fit.summary:
+fpga:
+ifeq ($(XILINX),1)
 ifeq ($(COMPILE_SERVER),localhost)
-	cp $(HW_DIR)/fpga/CYCLONEV-GT/output_files/$@ .
+	cp $(HW_DIR)/fpga/$(FPGA_FAMILY)/$(XIL_LOG) .
 else
-	scp $(IOB_USER)@$(COMPILE_SERVER):$(REMOTE_ROOT_DIR)/hardware/fpga/CYCLONEV-GT/output_files/*.fit.summary $@ 
+	scp $(COMPILE_USER)@$(COMPILE_SERVER):$(COMPILE_DIR)/$(XIL_LOG) .
 endif
-
-vivado.log:
+endif
+ifeq ($(INTEL),1)
 ifeq ($(COMPILE_SERVER),localhost)
-	cp $(HW_DIR)/fpga/XCKU/$@ .
+	cp $(HW_DIR)/fpga/$(FPGA_FAMILY)/output_files/$(INT_LOG) .
 else
-	scp $(IOB_USER)@$(COMPILE_SERVER):$(REMOTE_ROOT_DIR)/hardware/fpga/XCKU/$@ .
+	scp $(COMPILE_USER)@$(COMPILE_SERVER):$(COMPILE_DIR)/output_files/*.fit.summary $(INT_LOG) 
 endif
-
+endif
+	$(EXPORT_LIST) $(TEX_DIR)/software/fpga2tex.sh
 
 texclean:
 	@rm -f *~ *.aux *.out *.log *.summary *_results*
 	@rm -f *.lof *.toc *.fdb_latexmk  ug.fls  *.lot *.txt
 	make -C ../figures clean
+	@rm -f $(TEX_SRC) $(addprefix ../figures/, $(TEX_FIG))
 
 pdfclean: clean
 	@rm -f *.pdf
 
-.PHONY: pb ug texclean pdfclean
+pb.pdf: pb.tex figures fpga copy_files
+	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	evince $@ &
+
+ug.pdf: $(SRC) figures fpga
+	git rev-parse --short HEAD > shortHash.txt
+	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	evince $@ &
+
+copy_files:
+	@cp $(addprefix $(TEX_DIR)/document/, $(TEX_SRC)) .
+
+.PHONY: clean texclean pdfclean fpga figures
