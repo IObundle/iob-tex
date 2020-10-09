10780a552d428a6bc7acdece182c9e5fac8d64ca
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-10-10 00:10:23 +0100

    fix for i2s

--- document/document.mk
+++ document/document.mk
@@ -1,35 +1,35 @@
-XIL_LOG:=vivado.log
-INT_LOG:=quartus.log
-EXPORT_LIST:=\
-CORE_NAME=$(CORE_NAME)\
+XIL_LOG=vivado.log
+INT_LOG=quartus.log
+EXPORT_LIST=\
 INTEL=$(INTEL)\
-XILINX=$(XILINX)
+XILINX=$(XILINX)\
+IS_CORE=$(IS_CORE)
 
 TEX:=$(TEX_DIR)/document
 TEX_SW_DIR:=$(TEX_DIR)/software
 
 pb.pdf: pb.tex figures fpga_res
-	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
-	$(EXPORT_LIST) pdflatex '\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
 	evince $@ &
 
 ug.pdf: $(SRC) figures fpga_res version.txt
 	git rev-parse --short HEAD > shortHash.txt
-#	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
-#	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
+	$(EXPORT_LIST) pdflatex '\def\TEX{$(TEX)}\def\XILINX{$(XILINX)}\def\INTEL{$(INTEL)}\input{$<}'
 	evince $@ &
 
 export TD_FIGS
 figures:
-	cp $(TEX_DIR)/document/figures/* ../figures
+	cp -u $(TEX_DIR)/document/figures/* ../figures
 	make -C ../figures
 
 fpga_res: $(FPGA_LIST)
 ifeq ($(XILINX),1)
-	cp $(HW_DIR)/fpga/XCKU/vivado.log .
+	cp $(HW_DIR)/fpga/vivado/XCKU/vivado.log .
 endif
 ifeq ($(INTEL),1)
-	cp $(HW_DIR)/fpga/CYCLONEV-GT/output_files/quartus.log .
+	cp $(HW_DIR)/fpga/quartus/CYCLONEV-GT/quartus.log .
 endif
 	$(EXPORT_LIST) $(TEX_DIR)/software/fpga2tex.sh
 
--- software/fpga2tex.sh
+++ software/fpga2tex.sh
@@ -2,24 +2,24 @@
 
 #altera
 if [ $INTEL = 1 ]; then \
-LOG=$CORE_NAME.fit.summary;\
-RES="alt_results.tex";\                 
-ALM=`grep ALM $LOG |grep -o '[0-9]*,\?[0-9]* \/' | sed s/'\/'//g`;\
-FF=`grep registers $LOG |grep -o '[0-9]*' | sed s/'\/'//g`;\
-DSP=`grep DSP $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g`;\
-BRAM=`grep RAM $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g`;\
-BRAMb=`grep 'block memory' $LOG |grep -o '[0-9]*,[0-9]* \/' | sed s/'\/'//g`;\
+LOG="quartus.log" ;\
+RES="alt_results.tex" ;\
+ALM=`grep ALM $LOG |grep -o '[0-9]*,\?[0-9]* \/' | sed s/'\/'//g` ;\
+FF=`grep registers $LOG |grep -o '[0-9]*' | sed s/'\/'//g` ;\
+DSP=`grep DSP $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g` ;\
+BRAM=`grep RAM $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g` ;\
+BRAMb=`grep 'block memory' $LOG |grep -o '[0-9]*,[0-9]* \/' | sed s/'\/'//g` ;\
 PIN=`grep pin $LOG |grep -o '[0-9]* \/' | sed s/'\/'//g`;\
-echo "ALM & $ALM \\\\ \\hline" > $RES;\
-echo "\rowcolor{iob-blue}"  >> $RES;\
-echo "FF & $FF  \\\\  \\hline"  >> $RES;\
-echo "DSP & $DSP \\\\ \\hline"  >> $RES;\
-echo "\rowcolor{iob-blue}"  >> $RES;\
-echo "BRAM blocks & $BRAM \\\\ \\hline"  >> $RES;\
-echo "BRAM bits & $BRAMb \\\\ \\hline"  >> $RES;\
-echo "\rowcolor{iob-blue}"  >> $RES;\
-if [$IS_CORE != 1]; then \
-echo "PIN & $PIN \\\\ \\hline"  >> $RES;\
+echo "ALM & $ALM \\\\ \\hline" > $RES ;\
+echo "\rowcolor{iob-blue}"  >> $RES ;\
+echo "FF & $FF  \\\\  \\hline"  >> $RES ;\
+echo "DSP & $DSP \\\\ \\hline"  >> $RES ;\
+echo "\rowcolor{iob-blue}"  >> $RES ;\
+echo "BRAM blocks & $BRAM \\\\ \\hline"  >> $RES ;\
+echo "BRAM bits & $BRAMb \\\\ \\hline"  >> $RES ;\
+echo "\rowcolor{iob-blue}"  >> $RES ;\
+if [ $IS_CORE != 1 ]; then \
+echo "PIN & $PIN \\\\ \\hline"  >> $RES ;\
 fi \
 fi
 
@@ -39,7 +39,7 @@ echo "$FF  \\\\  \\hline" >> $RES ;\
 echo "$DSP \\\\ \\hline" >> $RES ;\
 echo "\rowcolor{iob-blue}" >> $RES ;\
 echo "$BRAM \\\\ \\hline" >> $RES ;\
-if [$IS_CORE != 1]; then \
-echo "$PIN \\\\ \\hline" >> $RES ; \
+if [ $IS_CORE != 1 ]; then \
+echo "$PIN \\\\ \\hline" >> $RES ;\
 fi \
 fi
