0d40e7ffdf6863183eb43317fb99d3a7dcc29b59
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-11-02 13:22:50 +0000

    suport system docs

--- document/document.mk
+++ document/document.mk
@@ -26,19 +26,31 @@ figures:
 	make -C ../figures
 
 fpga_res:
+	echo $(IS_CORE)
 ifeq ($(XILINX),1)
+ifeq ($(IS_CORE),1)
 	cp $($(CORE_NAME)_HW_DIR)/fpga/vivado/XCKU/vivado.log .
+else
+	cp $(HW_DIR)/fpga/AES-KU040-DB-G/vivado.log .
+endif
 endif
 ifeq ($(INTEL),1)
+ifeq ($(IS_CORE),1)
 	cp $($(CORE_NAME)_HW_DIR)/fpga/quartus/CYCLONEV-GT/quartus.log .
+else
+	cp $(HW_DIR)/fpga/CYCLONEV-GT-DK/top_system.fit.summary quartus.log
+endif
 endif
 	$(EXPORT_LIST) $(TEX_SW_DIR)/fpga2tex.sh
 
 texclean:
-	@rm -f *~ *.aux *.out *.log *.summary *_results*
+	@rm -f *~ *.aux *.out *.log *.summary 
 	@rm -f *.lof *.toc *.fdb_latexmk  ug.fls  *.lot *.txt
 	make -C ../figures clean
 	@rm -f $(TEX_SRC) $(addprefix ../figures/, $(TEX_FIG))
+
+resultsclean:
+	@rm *_results*
 
 pdfclean: clean
 	@rm -f *.pdf
